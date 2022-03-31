a32920bc7673b2ec1086bef00719492659acc8a6
nicat.aqayev3 <nicat.aqayev3@gmail.com>
2022-03-31 12:57:54 +0400

    31.03.2022

---
+++ Pustok/Pustok/wwwroot/js/main.js
@@ -0,0 +1,66 @@
+﻿$(document).ready(function () {
+    $(document).on("click", "#productdetailId", function (e) {
+        e.preventDefault();
+        let url = $(this).attr("href");
+        //console.log(url);
+
+        fetch(url).then(res => { return res.text() }).then(data =>
+        {
+            //console.log(data)
+
+            $("#quickModal .product-details-modal").html(data);
+            $("#quickModal").modal(true);
+
+        })
+    })
+
+    $(document).on("click", ".addtobasket", function (e) {
+        e.preventDefault();
+
+        let url = $(this).attr("href");
+        console.log(url)
+        //let quantity = $(".quantity").val();
+        //console.log(quantity)
+
+        fetch(url).then(res =>
+        {
+            //console.log(res)
+
+            //if (res.ok)
+            //{
+            //    alert("Sebete Elave Olundu")
+            //}
+
+            return res.text();
+        }).then(data =>
+        {
+            console.log(data)
+            $(".cart-block").html(data);
+        })
+
+    })
+
+    $(document).on("keyup", ".productcount", function () {
+        let count = $(this).val();
+        let productId = $(this).attr("data-productId");
+        let url = `/Product/AddToBasket/`;
+
+
+        fetch("/Product/ChangeBasketProductCount/" + productId + '?count=' + count).then(res => {
+            return res.text();
+        }).then(data => {
+            $(".productTable").html(data);
+        })
+
+
+        fetch("/Product/Salam/" + productId + '?count=' + count).then(res => {
+            return res.text();
+        }).then(data => {
+            console.log(data)
+            $(".cart-block").html(data);
+            $(".cart-summary .text-primary").html($(".cart-block .cart-total .price").text())
+        })
+    })
+
+    
+})
